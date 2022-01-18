7d4b85d171d5b1f72258fc8ee612558ee1b6ded1
theabhayprajapati <theabhayprajapati@gmail.com>
2022-01-18 15:23:30 +0530

    learning aboutpayents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import '@stripe/stripe-js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-function App() {
+import Checkout from './components/Checkout'
+import Success from './components/Success'
+import Cancel from './components/Cancel'
+
+import './styles.css'
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route index element={<Checkout />} />
+          <Route path="success" element={<Success />} />
+          <Route path="cancel" element={<Cancel />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
